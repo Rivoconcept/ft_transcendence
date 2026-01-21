c34cf4c4279cc4687b8d419994bc11ae2ef6d719
Rivoconcept <rivo.hanitrarivelo@gmail.com>
2026-01-21 09:29:07 +0300

    cardGame

--- srcs/frontend/src/main.tsx
+++ srcs/frontend/src/main.tsx
@@ -1,11 +1,14 @@
 // src/main.tsx
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import './styles/main.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './styles/main.scss';
+import { CardContextProvider } from "./context/CardContext";
+import CardScene from "./cardScenes/CardScene";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+      <CardContextProvider>
+        <CardScene />
+      </CardContextProvider>
   </React.StrictMode>
-)
+);
