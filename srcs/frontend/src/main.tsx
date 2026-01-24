75f33840c7e10ea9611381637bbd6ec42789ec83
safidy3 <safidy.anyaina@gmail.com>
2026-01-24 15:58:43 +0300

    feat : main menue

--- srcs/frontend/src/main.tsx
+++ srcs/frontend/src/main.tsx
@@ -1,14 +1,20 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './styles/main.scss';
-import { CardContextProvider } from "./context/CardContext";
-import CardScene from "./cardScenes/CardScene";
+
+import './Card-game/styles/main.scss';
+import { CardContextProvider } from "./Card-game/context/CardContext";
+import CardScene from "./Card-game/cardScenes/CardScene";
+
+import MainMenu from "./mainMenu";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-      <CardContextProvider>
-        <CardScene />
-      </CardContextProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<MainMenu />
+		{/* <CardContextProvider>
+			<CardScene />
+		</CardContextProvider> */}
+	</React.StrictMode>
 );
