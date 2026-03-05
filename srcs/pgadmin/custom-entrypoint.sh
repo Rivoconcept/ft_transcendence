b9b3b816ad8bce4f086dbc3442f7170ac2f18efd
erijania <erijania@student.42antananarivo.mg>
2026-03-05 09:15:35 +0300

    feat: update environment configuration for PostgreSQL and PgAdmin, add custom entrypoint script, and create .env.example files for backend and frontend

---
+++ srcs/pgadmin/custom-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cat > /tmp/servers.json <<EOF
+{
+  "Servers": {
+    "1": {
+      "Name": "${PGADMIN_SERVER_NAME}",
+      "Group": "Servers",
+      "Host": "${DB_HOST}",
+      "Port": ${DB_PORT},
+      "MaintenanceDB": "${POSTGRES_DB}",
+      "Username": "${POSTGRES_USER}",
+      "SSLMode": "prefer"
+    }
+  }
+}
+EOF
+
+exec /entrypoint.sh
