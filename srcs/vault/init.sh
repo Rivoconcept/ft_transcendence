e622999e76ec08fcc83a752978986270f4a50df7
tramanan <tramanan@kali>
2026-03-24 16:35:40 +0300

    feat : add docker for vault and restore docker for mode prod and dev

--- srcs/vault/init.sh
+++ srcs/vault/init.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
+set -x
 
-JWT_SECRET=$(grep JWT_SECRET /run/secrets/vault-init | cut -d= -f2)
-REFRESH_SECRET=$(grep REFRESH_SECRET /run/secrets/vault-init | cut -d= -f2)
-POSTGRES_PASSWORD=$(grep POSTGRES_PASSWORD /run/secrets/vault-init | cut -d= -f2)
-PGADMIN_DEFAULT_PASSWORD=$(grep PGADMIN_DEFAULT_PASSWORD /run/secrets/vault-init | cut -d= -f2)
+JWT_SECRET=$(grep JWT_SECRET /vault/vault-init | cut -d= -f2)
+REFRESH_SECRET=$(grep REFRESH_SECRET /vault/vault-init | cut -d= -f2)
+POSTGRES_PASSWORD=$(grep POSTGRES_PASSWORD /vault/vault-init | cut -d= -f2)
+PGADMIN_DEFAULT_PASSWORD=$(grep PGADMIN_DEFAULT_PASSWORD /vault/vault-init | cut -d= -f2)
 
 vault kv put secret/GameHub \
   JWT_SECRET=$JWT_SECRET \
@@ -15,13 +16,13 @@ vault kv put secret/GameHub \
 vault kv get -field=POSTGRES_PASSWORD secret/GameHub > /run/secrets/POSTGRES_PASSWORD
 vault kv get -field=PGADMIN_DEFAULT_PASSWORD secret/GameHub > /run/secrets/PGADMIN_DEFAULT_PASSWORD
 
+shred -u /vault/vault-init 2>/dev/null || true
+
+echo '✅ vault-init done'
+
 # for field in 
 # POSTGRES_PASSWORD
 #  PGADMIN_DEFAULT_PASSWORD
 #  ; do
 #   vault kv get -field=$field secret/GameHub > /run/secrets/$field
 # done
-
-shred -u /run/secrets/vault-init 2>/dev/null || true
-
-echo '✅ vault-init done'
